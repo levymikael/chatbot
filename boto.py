fbb8cc9d982a847c87807169d4bf534078a74880
levymikael <levymikael@gmail.com>
2019-07-04 14:33:34 +0300

    1st push

--- boto.py
+++ boto.py
@@ -1,19 +1,39 @@
 """
 This is the template server side for ChatBot
 """
+
 from bottle import route, run, template, static_file, request
 import json
 
 
 @route('/', method='GET')
 def index():
+
     return template("chatbot.html")
 
-
+# this is the line
 @route("/chat", method='POST')
 def chat():
-    user_message = request.POST.get('msg')
-    return json.dumps({"animation": "inlove", "msg": user_message})
+    user_input = request.POST.get('msg')
+    print(user_input)
+    input_list = user_input.split()
+    print(input_list)
+    swear_words_list = ["arse","ass","asshole",",bastard","bitch","bollocks","child-fucker","Christ on a bike","Christ on a cracker","crap",\
+                        "cunt","damn","frigger","fuck","goddamn","godsdamn","hell","holy shit","horseshit""Jesus Christ","Jesus fuck","Jesus H. Christ",\
+                        "Jesus Harold Christ,","Jesus wept","Jesus, Mary and Joseph","Judas Priest","motherfucker","nigga","nigger","prick","shit",\
+                        "shit ass","shitass","slut","son of a bitch","son of a motherless goat","son of a whore","sweet Jesus","twat"]
+    for index, word in enumerate(input_list):
+        print(input_list[index])
+        if input_list[index] == "love":
+            return_message = "Let's spread love together"
+            gif_name = "inlove"
+        elif input_list[index] == "animal" or "puppy" or "dog"
+        else:
+            return_message = "Sorry, I didn't understand what you just typed"
+            gif_name = "confused"
+
+
+    return json.dumps({"animation": gif_name, "msg": return_message})
 
 
 @route("/test", method='POST')
@@ -24,6 +44,7 @@ def chat():
 
 @route('/js/<filename:re:.*\.js>', method='GET')
 def javascripts(filename):
+
     return static_file(filename, root='js')
 
 
